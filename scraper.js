0f3933cdb8d5d116833c47972843762c2806904a
Rohan Natraj <ron4trj@gmail.com>
2014-04-22 04:30:17 -0400

    modify scraper for news data

--- scraper.js
+++ scraper.js
@@ -3,13 +3,24 @@
 var request=require("request");
 var cheerio=require("cheerio");
 
-var url= "http://www.cryptocurrencychart.com/";
+var url= "https://coinmarketcap.com/";
+
+var coinInfo=[];
+var coinName;
+var coinVal;
+var coinSupply;
 
 request(url, function(err, resp, body) {
-        if (err) {
-            console.log(err);
-        }
-        else {
-        	$ = cheerio.load(body);
-        }
-    });
+    if (err) {
+       console.log(err);
+    }
+    else {
+      	$ = cheerio.load(body);
+       	fillInfo();
+    }
+});
+
+function fillInfo() {
+	
+	console.log($("no-wrap currency-name a").text());
+}
